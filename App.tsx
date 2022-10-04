b0d27cf434122c756ff6e8340fc1429a68c0ef8f
Diogo V Marques <dimarques@gmail.com>
2022-10-04 12:37:26 -0300

    Estruturação inicial do projet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.eventName}>
+        Nome do Evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
     </View>
   );
 }
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      backgroundColor: '#131016',
+      padding: 24
   },
+  eventName: {
+    color: '#fdfcfe',
+    fontSize:24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16
+  }
 });

05b64dde83472108c6cb4ba0790bd68ee69c362e
Diogo V Marques <dimarques@gmail.com>
2022-10-04 12:46:31 -0300

    Refatoração da estrutura e lógica nas pastas

--- App.tsx
+++ App.tsx
@@ -1,32 +1,7 @@
-import { Text, View, StyleSheet } from 'react-native';
+import Home from './src/screens/Home';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do Evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022
-      </Text>
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-  },
-  eventName: {
-    color: '#fdfcfe',
-    fontSize:24,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6b6b6b',
-    fontSize: 16
-  }
-});
